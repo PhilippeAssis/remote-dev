fd90b263b97e67d0b271ca1c05daa8fd3435a00c
Philippe Assis <philippe.assis@zoom.com.br>
2021-04-19 17:38:03 -0300

    sync

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
-sudo apt upgrade && sudo apt update
+sudo apt upgrade && sudo apt update -y
 sudo apt install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
