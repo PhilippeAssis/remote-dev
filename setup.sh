25878e7db60b594110c23ca9fe578543ed389937
Philippe Assis <philippe.assis@zoom.com.br>
2021-04-19 17:47:18 -0300

    sync

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-export DEBIAN_FRONTEND=noninteractive
+echo Set Git user.name?
+read git_user_name
+echo Set Git user.email?
+read git_user_email
+
+git config --global core.editor code
+git config --global user.name code
+git config --global user.email code
+
+
 sudo apt upgrade && sudo apt update -y
-sudo apt install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sudo apt-get install build-essential;
+# RUST
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
+# Extras
+sudo apt install zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
