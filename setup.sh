7f193cabf1df7f9ea8800ed1681e62cbbbc0a0b2
Philippe Assis <philippe.assis@zoom.com.br>
2021-04-19 17:36:37 -0300

    sync

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt upgrade && sudo apt update
-sudo apt install zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sudo apt install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
